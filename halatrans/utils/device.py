fbcd8fb6c01635c444ffd71542c5671f6477182d
Hiram Deng <dzhsurf@gmail.com>
2024-06-20 23:35:26 -0700

    Update device.py

--- halatrans/utils/device.py
+++ halatrans/utils/device.py
@@ -1,11 +1,13 @@
-import pyaudio
 import logging
-from typing import Optional, Dict
+from typing import Dict, Optional
+
+import pyaudio
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-def get_audio_device_dict() -> Dict[int, str]: 
+
+def get_audio_device_dict() -> Dict[int, str]:
     p = pyaudio.PyAudio()
     device_dict = {}
     for i in range(p.get_device_count()):
@@ -13,11 +15,12 @@ def get_audio_device_dict() -> Dict[int, str]:
         device_dict[i] = info["name"]
     return device_dict
 
+
 def get_audio_device_index() -> Optional[int]:
     device_dict = get_audio_device_dict()
     selected_device: Optional[int] = None
     for idx, name in device_dict.items():
         if name.lower().strip() == "soundflower (2ch)":
             selected_device = idx
-            break 
+            break
     return selected_device
